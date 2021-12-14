9727350eb3921f00d2c67957d15675ca186a17f4
lioradopacio <liora.vanessa@gmail.com>
2021-12-14 17:30:08 -0300

    alteração nome função para não dar conflito com variável

--- principal_forca_multiplayer.py
+++ principal_forca_multiplayer.py
@@ -4,8 +4,8 @@ def main():
     jogando = True
     while jogando:
         funções_forca.jogar()
-        funções_forca.p1jogando()
-        funções_forca.p2jogando()
+        funções_forca.p1_jogando()
+        funções_forca.p2_jogando()
 
 if(__name__ == '__main__'):
     main()
